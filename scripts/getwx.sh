2c6ffbee8786eb397647a13daa869ae935536f05
Maciej Krok <vonproteus+sarah.local@gmail.com>
2016-12-26 20:41:15 +0100

    get data from aprs.fi

---
+++ scripts/getwx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+NAMETAG=$1
+WXNAME=$2
+WXAPIKEY=$3
+
+WXAPRS="http://api.aprs.fi/api/get?name=$WXNAME&apikey=$WXAPIKEY&format=xml&what=wx"
+WXFILE="/tmp/aprs.wx.xml"
+TIMECASH=$(( 60 * 60 * 5 )) # 5  minut
+
+
+NOW=$(date -u +%s)
+LASTTIME=$(xml sel -t -v "/xml/entries/entry/time" $WXFILE )
+
+if (( $NOW - $TIMECASH > $LASTTIME +1 ))
+then
+	wget $WXAPRS -O $WXFILE
+fi
+
+xml sel -t -v "/xml/entries/entry/$NAMETAG" $WXFILE
