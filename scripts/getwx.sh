0ce4b543c4ce5a07bc85e276e07f1f38d0dacce9
Maciej Krok <vonproteus+sarah.local@gmail.com>
2016-12-26 22:37:23 +0100

    time fix

--- scripts/getwx.sh
+++ scripts/getwx.sh
@@ -6,14 +6,15 @@ WXAPIKEY=$3
 
 WXAPRS="http://api.aprs.fi/api/get?name=$WXNAME&apikey=$WXAPIKEY&format=xml&what=wx"
 WXFILE="/tmp/aprs.wx.xml"
-TIMECASH=$(( 60 * 60 * 5 )) # 5  minut
+TIMECASH=$(( 60 * 10 )) # 5  minut
 
 
-NOW=$(date -u +%s)
-LASTTIME=$(xmlstarlet sel -t -v "/xml/entries/entry/time" $WXFILE )
+NOW=$(( $(date -u +%s) - $TIMECASH ))
+LASTTIME=$(( $(xmlstarlet sel -t -v "/xml/entries/entry/time" $WXFILE ) + 1 ))
 
-if (( $NOW - $TIMECASH > $LASTTIME +1 ))
+if (( $NOW > $LASTTIME )) 
 then
+	rm $WXFILE
 	wget $WXAPRS -O $WXFILE
 fi
 
