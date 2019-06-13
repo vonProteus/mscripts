b9631e016e9c9d9eed27e4cf6225218631e02cf8
Maciej Krok <maciej.krok@silvermedia.pl>
2019-06-13 08:55:37 +0200

    portainer update

--- scripts/makeportainer.sh
+++ scripts/makeportainer.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+docker stop portainer portainer_agent 
+docker rm portainer portainer_agent
+docker image rm portainer/portainer portainer/agent
+
 docker volume create portainer_data
 docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --name portainer --restart always portainer/portainer
 docker run -d -p 9001:9001 -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes --name portainer_agent --restart always portainer/agent
